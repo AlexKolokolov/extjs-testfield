f143f88bd78b270e741ff0c6724e28bf6b3f1bd8
AlexKolokolov <kolokolov_a@ukr.net>
2017-08-04 14:18:15 +0300

    Add ExtJS in action first example

---
+++ hello-world.js
@@ -0,0 +1,49 @@
+var tp1 = Ext.create('Ext.Template', [
+	'Hello {firstName} {lastName}!',
+	' Nice to meet you!'
+]);
+
+var formPanel = Ext.create('Ext.form.FormPanel', {
+	itemId: 'formPanel',
+	frame: true,
+	layout: 'anchor',
+	defaultType: 'textfield',
+	defaults: {
+		anchor: '-10',
+		labelWidth: 65
+	},
+	items: [
+		{
+			fieldLabel: 'First name',
+			name: 'firstName'
+		},
+		{
+			fieldLabel: 'Last name',
+			name: 'lastName'
+		}
+	],
+	buttons: [
+		{
+			text: 'Submit',
+			handler: function() {
+				var formPanel = this.up('#formPanel'),
+					vals = formPanel.getValues(),
+					greeting = tp1.apply(vals);
+				Ext.Msg.alert('Hello!',greeting);
+			}
+		}
+	]
+});
+
+Ext.onReady(function(){
+	Ext.create('Ext.window.Window',{
+		height: 125,
+		width: 200,
+		closable: false,
+		resizable: false,
+		title: 'Input needed.',
+		border: false,
+		layout: 'fit',
+		items: formPanel
+	}).show();
+});
